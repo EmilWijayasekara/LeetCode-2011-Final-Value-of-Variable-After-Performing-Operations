e40b377b8421441c580f70a85c01639e605dac8d
Emil <132345514+EmilWijayasekara@users.noreply.github.com>
2024-01-17 22:22:45 +0530

    Update 2011.final-value-of-variable-after-performing-operations.java

--- 2011.final-value-of-variable-after-performing-operations.java
+++ 2011.final-value-of-variable-after-performing-operations.java
@@ -1,14 +1,12 @@
-/*
- * @lc app=leetcode id=2011 lang=java
- *
- * [2011] Final Value of Variable After Performing Operations
- */
-
-// @lc code=start
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
-        
+        int xValue = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if(operations[i].equals("--X")) --xValue;
+            if(operations[i].equals("X++")) xValue++;
+            if(operations[i].equals("X--")) xValue--;
+            if(operations[i].equals("++X")) ++xValue;
+        }
+        return xValue;
     }
 }
-// @lc code=end
-
